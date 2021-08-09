8a0d2c7f76c0149f4813577858ec98b534aa91b5
alexivkov <sivkov@inbox.ru>
2021-08-09 08:46:28 +0300

    feat: add function reverse and refactor in that commit - it is a mistake

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) { 
+    let sum = 0;
+    if (n < 0) {
+        n = -n;
+    }
+    while(n > 0) {
+      sum = (sum * 10) + (n % 10);//7 75
+      n = Math.floor(n / 10);
+  }  
+  return sum;
 }
